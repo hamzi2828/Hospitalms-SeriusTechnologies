2a605c1910c948e96414f3c6ca85beffbf222284
hamza jahangir <hamzahashmi640@gmail.com>
2025-01-06 15:18:53 +0500

    c

--- application/views/includes/admin/death-certificates.php
+++ application/views/includes/admin/death-certificates.php
@@ -37,48 +37,24 @@
         <span class="arrow "></span>
     </a>
     <ul class="sub-menu">
-        <?php if (!empty($access) and !in_array('all-products', explode(',', $access->access))) : ?>
-            <li class="<?php if ($child_uri == 'all-products') echo 'active'; ?>">
-                <a href="<?php echo base_url('/cafe-setting/all-products') ?>">
-                    All Products
+
+    <?php if (!empty($access) and !in_array('all-sale', explode(',', $access->access))) : ?>
+            <li class="<?php if ($child_uri == 'all-sale') echo 'active'; ?>">
+                <a href="<?php echo base_url('/cafe-setting/all-sale') ?>">
+                    All Sale
                 </a>
             </li>
         <?php endif; ?>
-        <?php if (!empty($access) and !in_array('add-product', explode(',', $access->access))) : ?>
-            <li class="<?php if ($child_uri == 'add-product') echo 'active'; ?>">
-                <a href="<?php echo base_url('/cafe-setting/add-product') ?>">
-                    Add Product
+
+        <?php if (!empty($access) and !in_array('add-sale', explode(',', $access->access))) : ?>
+            <li class="<?php if ($child_uri == 'add-sale') echo 'active'; ?>">
+                <a href="<?php echo base_url('/cafe-setting/add-sale') ?>">
+                    Add Sale
                 </a>
             </li>
         <?php endif; ?>
-        <?php if (!empty($access) and !in_array('all-category', explode(',', $access->access))) : ?>
-            <li class="<?php if ($child_uri == 'all-category') echo 'active'; ?>">
-                <a href="<?php echo base_url('/cafe-setting/all-category') ?>">
-                    All Category
-                </a>
-            </li>
-        <?php endif; ?>
-        <?php if (!empty($access) and !in_array('add-category', explode(',', $access->access))) : ?>
-            <li class="<?php if ($child_uri == 'add-category') echo 'active'; ?>">
-                <a href="<?php echo base_url('/cafe-setting/add-category') ?>">
-                    Add Category
-                </a>
-            </li>
-        <?php endif; ?>
-        <?php if (!empty($access) and !in_array('all-ingredients', explode(',', $access->access))) : ?>
-            <li class="<?php if ($child_uri == 'all-ingredients') echo 'active'; ?>">
-                <a href="<?php echo base_url('/cafe-setting/all-ingredients') ?>">
-                    All Ingredients
-                </a>
-            </li>
-        <?php endif; ?>
-        <?php if (!empty($access) and !in_array('add-ingredients', explode(',', $access->access))) : ?>
-            <li class="<?php if ($child_uri == 'add-ingredients') echo 'active'; ?>">
-                <a href="<?php echo base_url('/cafe-setting/add-ingredients') ?>">
-                    Add Ingredients
-                </a>
-            </li>
-        <?php endif; ?>
+
+
 
         <?php if (!empty($access) and !in_array('all-stock', explode(',', $access->access))) : ?>
             <li class="<?php if ($child_uri == 'all-stock') echo 'active'; ?>">
@@ -94,20 +70,69 @@
                 </a>
             </li>
         <?php endif; ?>
-        <?php if (!empty($access) and !in_array('add-sale', explode(',', $access->access))) : ?>
-            <li class="<?php if ($child_uri == 'add-sale') echo 'active'; ?>">
-                <a href="<?php echo base_url('/cafe-setting/add-sale') ?>">
-                    Add Sale
+
+
+
+
+
+
+
+        <?php if (!empty($access) and !in_array('all-products', explode(',', $access->access))) : ?>
+            <li class="<?php if ($child_uri == 'all-products') echo 'active'; ?>">
+                <a href="<?php echo base_url('/cafe-setting/all-products') ?>">
+                    All Products
+                </a>
+            </li>
+        <?php endif; ?>
+        <?php if (!empty($access) and !in_array('add-product', explode(',', $access->access))) : ?>
+            <li class="<?php if ($child_uri == 'add-product') echo 'active'; ?>">
+                <a href="<?php echo base_url('/cafe-setting/add-product') ?>">
+                    Add Product
                 </a>
             </li>
         <?php endif; ?>
 
-        <?php if (!empty($access) and !in_array('all-sale', explode(',', $access->access))) : ?>
-            <li class="<?php if ($child_uri == 'all-sale') echo 'active'; ?>">
-                <a href="<?php echo base_url('/cafe-setting/all-sale') ?>">
-                    All Sale
+
+
+
+
+
+        <?php if (!empty($access) and !in_array('all-category', explode(',', $access->access))) : ?>
+            <li class="<?php if ($child_uri == 'all-category') echo 'active'; ?>">
+                <a href="<?php echo base_url('/cafe-setting/all-category') ?>">
+                    All Category
                 </a>
             </li>
         <?php endif; ?>
+        <?php if (!empty($access) and !in_array('add-category', explode(',', $access->access))) : ?>
+            <li class="<?php if ($child_uri == 'add-category') echo 'active'; ?>">
+                <a href="<?php echo base_url('/cafe-setting/add-category') ?>">
+                    Add Category
+                </a>
+            </li>
+        <?php endif; ?>
+
+
+
+        
+        <?php if (!empty($access) and !in_array('all-ingredients', explode(',', $access->access))) : ?>
+            <li class="<?php if ($child_uri == 'all-ingredients') echo 'active'; ?>">
+                <a href="<?php echo base_url('/cafe-setting/all-ingredients') ?>">
+                    All Ingredients
+                </a>
+            </li>
+        <?php endif; ?>
+        <?php if (!empty($access) and !in_array('add-ingredients', explode(',', $access->access))) : ?>
+            <li class="<?php if ($child_uri == 'add-ingredients') echo 'active'; ?>">
+                <a href="<?php echo base_url('/cafe-setting/add-ingredients') ?>">
+                    Add Ingredients
+                </a>
+            </li>
+        <?php endif; ?>
+
+
+
+
+
     </ul>
 </li>
