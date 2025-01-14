0ed71fac284bde23ca2425b3ecd4b1d73d4c2d64
hamza jahangir <hamzahashmi640@gmail.com>
2025-01-14 14:52:55 +0500

    c

--- application/controllers/CafeSetting.php
+++ application/controllers/CafeSetting.php
@@ -1054,7 +1054,7 @@ class CafeSetting extends CI_Controller {
         }
 
 
-         $ledger_description = 'Cafe Inventory added. Invoice# ' . $new_invoice_id;
+         $ledger_description = 'Cafe Inventory refunded. Invoice# ' . $new_invoice_id;
 
             $mm_ledger = array (
                 'user_id'          => get_logged_in_user_id (),
@@ -1063,9 +1063,9 @@ class CafeSetting extends CI_Controller {
                 'invoice_id'       => $new_invoice_id,
                 'payment_mode'     => 'cash',
                 'paid_via'         => '',
-                'transaction_type' => 'debit',
-                'credit'           => 0,
-                'debit'            => $total_sale_quantity_and_tp_unit,
+                'transaction_type' => 'credit',
+                'credit'           => $total_sale_quantity_and_tp_unit,
+                'debit'            => 0,
                 'description'      => $ledger_description,
                 'trans_date'       => date ( 'Y-m-d' ),
                 'date_added'       => current_date_time ()
