2c48972bff813fcef08e79997ec39d83d6b8f993
hamza jahangir <hamzahashmi640@gmail.com>
2025-01-24 11:43:59 +0500

    cc

--- application/views/invoices/format-cp-peripheral-film-report.php
+++ application/views/invoices/format-cp-peripheral-film-report.php
@@ -18,268 +18,82 @@
     </tbody>
 </table>
 
-<table class="items" width="100%" style="font-size: 9pt; border-collapse: collapse; margin-top: 0; border: 0"
+<table class="items" width="100%" style="font-size: 10pt; border-collapse: collapse; margin-top: 0; border: 0"
        cellpadding="4" border="0">
     <thead>
-    <tr style="background: #f5f5f5;">
-    
-        <th align="left" style="width: 30%;">Test Name</th>
-        <th align="left" style="width: 10%;">Results</th>
-        <th colspan="2" align="center" style="width: 20%;">Previous Results</th>
-        <th align="left" style="width: 10%;">Units</th>
-        <th align="left" style="width: 20%;">Reference Ranges</th>
-    </tr>
+        <tr style="background: #f5f5f5;">
+            <th align="left" style="width: 30%;">Test Name</th>
+            <th align="left" style="width: 10%;">Results</th>
+            <th colspan="2" align="center" style="width: 20%;">Previous Results</th>
+            <th align="left" style="width: 10%;">Units</th>
+            <th align="left" style="width: 20%;">Reference Ranges</th>
+        </tr>
     </thead>
     <tbody>
-    <!-- ITEMS HERE -->
-    <td align="left" colspan="2" width="35%">
-    </td>
-    <tr>
-   
-        
         <?php
-            if ( count ( $previous_results ) > 0 ) {
-                foreach ( $previous_results as $key => $previous_result ) {
-                    ?>
-                    <td align="center">
-                        <?php echo date ( 'd-m-Y', strtotime ( $previous_result -> date_added ) ) ?>
+        if (count(cp_peripheral_film_general) > 0) {
+            foreach (cp_peripheral_film_general as $test_id) {
+                $test_info = get_test_by_id($test_id);
+                $unit = get_test_unit_id_by_id($test_id);
+                $ranges = get_reference_ranges_by_test_id($test_id);
+                $result = get_test_results($sale_id, $test_id);
+                $previous_results = get_previous_test_results($sale_id, $test_id);
+                ?>
+                <tr>
+                    <!-- Test Name -->
+                    <td align="left" style="font-size: 9pt" width="35%">
+                        <?php echo $test_info->report_title ?>
                     </td>
+                    <!-- Results with abnormal check -->
+                    <td align="center" style="font-size: 9pt; <?php if ($result->abnormal == '1') echo 'color: #FF0000; font-weight: bold;' ?>">
+                        <?php echo $result->result ?>
+                    </td>
+                    <!-- Previous Results -->
                     <?php
-                }
-                $td = 2 - count ( $previous_results );
-                for ( $loop = 1; $loop <= $td; $loop++ ) {
-                    echo '<td></td>';
-                }
+                    if (count($previous_results) > 0) {
+                        foreach ($previous_results as $previous_result) {
+                            ?>
+                            <td style="font-size: 9pt" align="center">
+                                <?php echo $previous_result->result; ?>
+                            </td>
+                            <?php
+                        }
+                        // Fill empty cells for remaining previous result columns
+                        $td = 2 - count($previous_results);
+                        for ($loop = 1; $loop <= $td; $loop++) {
+                            echo '<td></td>';
+                        }
+                    } else {
+                        echo '<td colspan="2"></td>';
+                    }
+                    ?>
+                    <!-- Units -->
+                    <td align="left" style="font-size: 9pt">
+                        <?php echo @get_unit_by_id($unit) ?>
+                    </td>
+                    <!-- Reference Ranges -->
+                    <td align="left" style="font-size: 9pt">
+                        <?php
+                        if (count($ranges) > 0) {
+                            foreach ($ranges as $range) {
+                                if (!empty(trim($range->gender))) {
+                                    echo '<b>' . ucwords(str_replace('f', 'F', $range->gender)) . '</b>: ';
+                                }
+                                if (!empty(trim($range->start_range)) && !empty(trim($range->end_range))) {
+                                    echo $range->start_range . '-' . $range->end_range . '<br/>';
+                                } elseif (!empty(trim($range->start_range))) {
+                                    echo $range->start_range . '<br/>';
+                                } elseif (!empty(trim($range->end_range))) {
+                                    echo $range->end_range . '<br/>';
+                                }
+                            }
+                        }
+                        ?>
+                    </td>
+                </tr>
+                <?php
             }
-            else
-                echo '<td colspan="2"></td>';
+        }
         ?>
-        
-        <td colspan="2"></td>
-    </tr>
-    <?php
-        if ( count ( cp_peripheral_film_general ) > 0 ) {
-            foreach ( cp_peripheral_film_general as $test_id ) {
-                $test_info        = get_test_by_id ( $test_id );
-                $unit             = get_test_unit_id_by_id ( $test_id );
-                $ranges           = get_reference_ranges_by_test_id ( $test_id );
-                $result           = get_test_results ( $sale_id, $test_id );
-                $previous_results = get_previous_test_results ( $sale_id, $test_id );
-                ?>
-                <tr>
-                    <td align="left" style="font-size: 9pt" width="35%">
-                        <?php echo $test_info -> report_title ?>
-                    </td>
-                    <td align="center" style="font-size: 9pt; <?php if ( $result -> abnormal == '1' )
-                        echo 'color: #FF0000; font-weight: bold' ?>">
-                        <?php echo $result -> result ?>
-                    </td>
-                    <?php
-                        if ( count ( $previous_results ) > 0 ) {
-                            foreach ( $previous_results as $previous_result ) {
-                                $previousResultTestIDS = get_child_tests_ids ( $previous_result -> test_id );
-                                $previousSubTests      = get_lab_test_results_by_ids ( $previous_result -> sale_id, $previousResultTestIDS -> ids, $previous_result -> id );
-                                ?>
-                                <td style="font-size: 9pt" align="center">
-                                    <?php echo $previous_result -> result; ?>
-                                </td>
-                                <?php
-                            }
-                            $td = 2 - count ( $previous_results );
-                            for ( $loop = 1; $loop <= $td; $loop++ ) {
-                                echo '<td></td>';
-                            }
-                        }
-                        else {
-                            $td = 2 - count ( $previous_results );
-                            for ( $loop = 1; $loop <= $td; $loop++ ) {
-                                echo '<td></td>';
-                            }
-                        }
-                    ?>
-                    <td align="left" style="font-size: 9pt">
-                        <?php echo @get_unit_by_id ( $unit ) ?>
-                    </td>
-                    <td align="left" style="font-size: 9pt">
-                        <?php
-                            if ( count ( $ranges ) > 0 ) {
-                                foreach ( $ranges as $range ) {
-                                    if ( !empty( trim ( $range -> gender ) ) )
-                                        echo '<b>' . ucwords ( str_replace ( 'f', 'F', $range -> gender ) ) . '</b>: ';
-                                    
-                                    if ( !empty( trim ( $range -> start_range ) ) and !empty( trim ( $range -> end_range ) ) )
-                                        echo $range -> start_range . '-' . $range -> end_range . '<br/>';
-                                    
-                                    else if ( !empty( trim ( $range -> start_range ) ) and empty( trim ( $range -> end_range ) ) )
-                                        echo $range -> start_range . '<br/>';
-                                    
-                                    else if ( empty( trim ( $range -> start_range ) ) and !empty( trim ( $range -> end_range ) ) )
-                                        echo $range -> end_range . '<br/>';
-                                    
-                                    else if ( empty( trim ( $range -> start_range ) ) and empty( trim ( $range -> end_range ) ) )
-                                        echo '';
-                                }
-                            }
-                        ?>
-                    </td>
-                </tr>
-                <?php
-            }
-        }
-    ?>
-    <tr>
-        <td colspan="6" style="font-size: 9pt; padding-top: 30px">
-            <strong>
-                <u>Diff. Leuc. Count (DLC)</u>
-            </strong>
-        </td>
-    </tr>
-    <?php
-        if ( count ( cp_peripheral_film_dlc ) > 0 ) {
-            foreach ( cp_peripheral_film_dlc as $test_id ) {
-                $test_info        = get_test_by_id ( $test_id );
-                $unit             = get_test_unit_id_by_id ( $test_id );
-                $ranges           = get_reference_ranges_by_test_id ( $test_id );
-                $result           = get_test_results ( $sale_id, $test_id );
-                $previous_results = get_previous_test_results ( $sale_id, $test_id );
-                ?>
-                <tr>
-                    <td align="left" style="font-size: 9pt" width="35%">
-                        <?php echo $test_info -> report_title ?>
-                    </td>
-                    <td align="center" style="font-size: 9pt; <?php if ( $result -> abnormal == '1' )
-                        echo 'color: #FF0000; font-weight: bold' ?>">
-                        <?php echo $result -> result ?>
-                    </td>
-                    <?php
-                        if ( count ( $previous_results ) > 0 ) {
-                            foreach ( $previous_results as $previous_result ) {
-                                $previousResultTestIDS = get_child_tests_ids ( $previous_result -> test_id );
-                                $previousSubTests      = get_lab_test_results_by_ids ( $previous_result -> sale_id, $previousResultTestIDS -> ids, $previous_result -> id );
-                                ?>
-                                <td style="font-size: 9pt" align="center">
-                                    <?php echo $previous_result -> result; ?>
-                                </td>
-                                <?php
-                            }
-                            $td = 2 - count ( $previous_results );
-                            for ( $loop = 1; $loop <= $td; $loop++ ) {
-                                echo '<td></td>';
-                            }
-                        }
-                        else {
-                            $td = 2 - count ( $previous_results );
-                            for ( $loop = 1; $loop <= $td; $loop++ ) {
-                                echo '<td></td>';
-                            }
-                        }
-                    ?>
-                    <td align="left" style="font-size: 9pt">
-                        <?php echo @get_unit_by_id ( $unit ) ?>
-                    </td>
-                    <td align="left" style="font-size: 9pt">
-                        <?php
-                            if ( count ( $ranges ) > 0 ) {
-                                foreach ( $ranges as $range ) {
-                                    if ( !empty( trim ( $range -> gender ) ) )
-                                        echo '<b>' . ucwords ( str_replace ( 'f', 'F', $range -> gender ) ) . '</b>: ';
-                                    
-                                    if ( !empty( trim ( $range -> start_range ) ) and !empty( trim ( $range -> end_range ) ) )
-                                        echo $range -> start_range . '-' . $range -> end_range . '<br/>';
-                                    
-                                    else if ( !empty( trim ( $range -> start_range ) ) and empty( trim ( $range -> end_range ) ) )
-                                        echo $range -> start_range . '<br/>';
-                                    
-                                    else if ( empty( trim ( $range -> start_range ) ) and !empty( trim ( $range -> end_range ) ) )
-                                        echo $range -> end_range . '<br/>';
-                                    
-                                    else if ( empty( trim ( $range -> start_range ) ) and empty( trim ( $range -> end_range ) ) )
-                                        echo '';
-                                }
-                            }
-                        ?>
-                    </td>
-                </tr>
-                <?php
-            }
-        }
-    ?>
-    <tr>
-        <td colspan="6" style="font-size: 9pt; padding-top: 30px">
-            <strong>
-                <u>Red Cell Morphology</u>
-            </strong>
-        </td>
-    </tr>
-    <?php
-        if ( count ( cp_peripheral_film_rcm ) > 0 ) {
-            foreach ( cp_peripheral_film_rcm as $test_id ) {
-                $test_info        = get_test_by_id ( $test_id );
-                $unit             = get_test_unit_id_by_id ( $test_id );
-                $ranges           = get_reference_ranges_by_test_id ( $test_id );
-                $result           = get_test_results ( $sale_id, $test_id );
-                $previous_results = get_previous_test_results ( $sale_id, $test_id );
-                ?>
-                <tr>
-                    <td align="left" style="font-size: 9pt" width="35%">
-                        <?php echo $test_info -> report_title ?>
-                    </td>
-                    <td align="center" style="font-size: 9pt; <?php if ( $result -> abnormal == '1' )
-                        echo 'color: #FF0000; font-weight: bold' ?>">
-                        <?php echo $result -> result ?>
-                    </td>
-                    <?php
-                        if ( count ( $previous_results ) > 0 ) {
-                            foreach ( $previous_results as $previous_result ) {
-                                $previousResultTestIDS = get_child_tests_ids ( $previous_result -> test_id );
-                                $previousSubTests      = get_lab_test_results_by_ids ( $previous_result -> sale_id, $previousResultTestIDS -> ids, $previous_result -> id );
-                                ?>
-                                <td style="font-size: 9pt" align="center">
-                                    <?php echo $previous_result -> result; ?>
-                                </td>
-                                <?php
-                            }
-                            $td = 2 - count ( $previous_results );
-                            for ( $loop = 1; $loop <= $td; $loop++ ) {
-                                echo '<td></td>';
-                            }
-                        }
-                        else {
-                            $td = 2 - count ( $previous_results );
-                            for ( $loop = 1; $loop <= $td; $loop++ ) {
-                                echo '<td></td>';
-                            }
-                        }
-                    ?>
-                    <td align="left" style="font-size: 9pt">
-                        <?php echo @get_unit_by_id ( $unit ) ?>
-                    </td>
-                    <td align="left" style="font-size: 9pt">
-                        <?php
-                            if ( count ( $ranges ) > 0 ) {
-                                foreach ( $ranges as $range ) {
-                                    if ( !empty( trim ( $range -> gender ) ) )
-                                        echo '<b>' . ucwords ( str_replace ( 'f', 'F', $range -> gender ) ) . '</b>: ';
-                                    
-                                    if ( !empty( trim ( $range -> start_range ) ) and !empty( trim ( $range -> end_range ) ) )
-                                        echo $range -> start_range . '-' . $range -> end_range . '<br/>';
-                                    
-                                    else if ( !empty( trim ( $range -> start_range ) ) and empty( trim ( $range -> end_range ) ) )
-                                        echo $range -> start_range . '<br/>';
-                                    
-                                    else if ( empty( trim ( $range -> start_range ) ) and !empty( trim ( $range -> end_range ) ) )
-                                        echo $range -> end_range . '<br/>';
-                                    
-                                    else if ( empty( trim ( $range -> start_range ) ) and empty( trim ( $range -> end_range ) ) )
-                                        echo '';
-                                }
-                            }
-                        ?>
-                    </td>
-                </tr>
-                <?php
-            }
-        }
-    ?>
     </tbody>
 </table>
