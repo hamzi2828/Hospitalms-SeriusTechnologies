de7a4f05d2a74bbb56dadaa68cd865d236f7ddcd
hamza jahangir <hamzahashmi640@gmail.com>
2025-02-19 12:36:46 +0500

    c

--- application/views/lab/load-sale-lab-tests.php
+++ application/views/lab/load-sale-lab-tests.php
@@ -1,49 +1,47 @@
 <div class="sale-<?php echo $row ?>" style="display: block; float: left; width: 100%; background: #f5f5f5; padding: 10px 0 0 15px; margin-bottom: 15px; position: relative">
     
-<div class="form-group col-lg-4" style="padding-left: 0">
+    <!-- Lab Test Section -->
+    <div class="form-group col-lg-4" style="padding-left: 0">
         <a href="javascript:void(0)" onclick="remove_lab_row(<?php echo $row ?>)">
             <i class="fa fa-trash"></i>
         </a>
         <label>Lab Test</label>
-        <input type="hidden" name="test_id[]" value="<?php echo $test -> id ?>">
-        <input type="text" class="form-control" readonly="readonly" value="<?php echo '('.$test -> code.') '.$test -> name ?>">
+        <input type="hidden" name="test_id[]" value="<?php echo $test->id ?>">
+        <input type="text" class="form-control" readonly="readonly" value="<?php echo '('.$test->code.') '.$test->name ?>">
     </div>
     
-    <div class="form-group col-lg-1" style="padding-left: 0">
+    <!-- Price Section -->
+    <div class="form-group col-lg-2" style="padding-left: 0; max-width: 100px">
         <label>Price</label>
         <input type="text" class="form-control price" readonly="readonly" name="price[]" value="<?php echo $price ?>">
     </div>
     
-    <div class="col-lg-3" style="display: flex; align-items: center; gap: 10px; padding-left: 0;">
-        <div>
-            <label><strong>Report Date & Time</strong></label>
-            <input type="datetime-local" name="report-collection-date-time[]" class="form-control"
-            value="<?php echo isset($default_datetime) ? $default_datetime : '' ?>">
-        </div>
-      
+    <!-- Report Date & Time Section -->
+    <div class="form-group col-lg-3" style="padding-left: 0; max-width: 160px">
+        <label><strong>Report Date & Time</strong></label>
+        <input type="datetime-local" 
+               name="report-collection-date-time[]" 
+               class="form-control"
+               value="<?php echo isset($default_datetime) ? $default_datetime : '' ?>"
+               placeholder="dd/mm/yyyy --:--">
     </div>
 
-        
-    <div class="form-group col-lg-2" style="padding-left: 0">
-            <div >
-            <label><strong>Due</strong></label>
-            <select name="due[]" class="form-control">
-                <option value="0">No</option>
-                <option value="1">Yes</option>
-            </select>
-        </div>
-    
+    <!-- Due Dropdown -->
+    <div class="form-group col-lg-2" style="padding-left: 5px; max-width: 100px">
+        <label><strong>Due</strong></label>
+        <select name="due[]" class="form-control">
+            <option value="0">No</option>
+            <option value="1">Yes</option>
+        </select>
     </div>
 
-    <div class="form-group col-lg-2" style="padding-left: 0">
-            <div >
-            <label><strong>Urgent</strong></label>
-            <select name="urgent[]" class="form-control">
-                <option value="0">No</option>
-                <option value="1">Yes</option>
-            </select>
-        </div>
-    
+    <!-- Urgent Dropdown -->
+    <div class="form-group col-lg-2" style="padding-left: 5px; max-width: 100px">
+        <label><strong>Urgent</strong></label>
+        <select name="urgent[]" class="form-control">
+            <option value="0">No</option>
+            <option value="1">Yes</option>
+        </select>
     </div>
 
 </div>
